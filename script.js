b5b570136eb93e60ecf8c1593be28d437fa0a532
mililca <141839434+mililca@users.noreply.github.com>
2023-09-06 17:05:27 +0200

    Add function to play a single round

--- script.js
+++ script.js
@@ -3,3 +3,45 @@ function getCompChoice() {
     const compChoice = Math.floor(Math.random() * options.length)
     return options[compChoice]
 }
+
+var playerChoice = ""
+var compChoice = ""
+
+function playRound(playerChoice, compChoice) {
+    playerChoice = prompt("Pick rock, paper or scissors: ")
+    playerChoice = playerChoice.toLowerCase()
+    compChoice = getCompChoice()
+    if (playerChoice === compChoice) {
+        return "draw"
+    }
+    else if (playerChoice === "rock") {
+        if (compChoice === "scissors") {
+            return "win"
+        }
+        else if (compChoice === "paper") {
+            return "loss"
+        }
+    }
+    else if (playerChoice === "scissors") {
+        if (compChoice === "paper") {
+            return "win"
+        }
+        else if (compChoice === "rock") {
+            return "loss"
+        }
+    }
+    else if (playerChoice === "paper") {
+        if (compChoice === "rock") {
+            return "win"
+        }
+        else if (compChoice === "scissors") {
+            return "loss"
+        }
+    }
+    else if (playerChoice !== "paper" || playerChoice !== "rock" || 
+    playerChoice !== "scissors") {
+        return "Pick form paper, rock or scissor next time!"
+    }
+}
+
+// console.log(playRound(playerChoice, getCompChoice()))
